eb7af22343a25c4a32855a0d87583760a7111418
admin <admin@localhost.localdomain>
2020-07-28 13:53:32 +0200

    tested compare events script

--- compare_events.py
+++ compare_events.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-table_1 = pd.read_csv()
-table_2 = pd.read_cvs()
+table_1 = pd.read_csv("/home/admin/Desktop/SiPM/results/HPK_coincidences/uncovered/horizontal_4ov_c1.csv")
+table_2 = pd.read_csv("/home/admin/Desktop/SiPM/results/HPK_coincidences/uncovered/horizontal_4ov_c2.csv")
 
 n_coincidences = 0
+coinc = 0
 
 for x in table_1['Timestamp']:
-	# these are all the similar ones
-	if table_2[ abs(table_2['Timestamp'] - x) < 0.01]:
-        n_coincidences += 1
+        coinc = len(table_2[ abs(table_2['Timestamp'] - x) < 0.00000001])
+        #print(coinc)
+        if coinc == 1: n_coincidences += 1
+        if coinc >1: print ("Too many")
 
 print(n_coincidences)
